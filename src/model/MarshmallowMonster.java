4335aa19ebccfcc9e12634f2c594fd0a12c90557
Bryson Spaulding <brysonian10@hotmail.com>
2018-09-12 08:07:45 -0600

    Added Getters and Setters, updated monster to allow change.

--- src/model/MarshmallowMonster.java
+++ src/model/MarshmallowMonster.java
@@ -32,7 +32,71 @@ public class MarshmallowMonster
 		this.armCount = armCount;
 	}
 	
-	public String toString()
+	//--------- Getters ---------
+	
+	public String getName()
+	{
+		
+		
+		return name;
+	}
+	
+	public double getlegCount()
+	{
+		
+		
+		return legCount;
+	}
+	
+	public int getEyeCount()
+	{
+		
+		
+		return eyeCount;
+	}
+	
+	public boolean getHasNoses()
+	{
+		
+		
+		return hasNoses;
+	}
+	
+	public int getArmCount()
+	{
+		
+		
+		return armCount;
+	}
+	
+	//--------- Setters ---------
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public void setLegCount(double legs)
+	{
+		this.legCount = legs;
+	}
+	
+	public void setEyeCount(int eyes)
+	{
+		this.eyeCount = eyes;
+	}
+	
+	public void setHasNoses(boolean hasNoses)
+	{
+		this.hasNoses = hasNoses;
+	}
+	
+	public void setArmCount(int arms)
+	{
+		this.armCount = arms;
+	}
+	
+ 	public String toString()
 	{
 		String description = "My monsters name is " + name + " It has " + legCount
 				 + " legs" + " and the scary thing is that it just stares at you,"
